4edb61d43327f1bc9bae5005488c0a44184e1367
pisanzapra <pisanzapra@users.noreply.github.com>
2022-05-14 18:25:22 +0100

    Installed Boostrap and SASS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import "./sass/main.scss";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import Home from "./Home";
 
 const App = () => {
